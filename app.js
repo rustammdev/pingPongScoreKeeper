deea873dd30a08543c250b0626b033ca6236a2df
Rustamjon Nuriddinov <rustamcheek11@gmail.com>
2023-12-30 22:28:00 +0500

    ended

--- app.js
+++ app.js
@@ -1,37 +1,67 @@
-const player1 = document.querySelector('#player1')
-const player2 = document.querySelector('#player2')
+const player1 = document.querySelector('#player1');
+const player2 = document.querySelector('#player2');
 
-const gameLavel = document.querySelector('#gameLavel')
+const gameLavel = document.querySelector('#gameLavel');
 
-const btnPlayer1 = document.querySelector('#btnPlayer1')
-const btnPlayer2 = document.querySelector('#btnPlayer2')
+const btnPlayer1 = document.querySelector('#btnPlayer1');
+const btnPlayer2 = document.querySelector('#btnPlayer2');
 
-const reset = document.querySelector('#reset')
+const reset = document.querySelector('#reset');
 
 // Winner point
 let selectValue = 3
 gameLavel.addEventListener('change', (e) => {
-    selectValue = e.target.value
+    selectValue = e.target.value;
+
+    player1.textContent = 0;
+    player2.textContent = 0;
+
+    a = 0, b = 0;
 })
 
 // Check the winner
 function check(player1, value) {
     if (player1 >= value){
-        return true
+        return true;
     }
 }
 
 // Change the value
-let a = 0
+var a = 0;
 btnPlayer1.addEventListener('click', () => {
-    a += 2
-    player1.textContent = a
+    player1.textContent = a;
+    if(check(a, selectValue)){
+        player1.style.color = 'green';
+        btnPlayer1.classList.add('disabled');
+        btnPlayer2.classList.add('disabled');
+        a = 0
+    }
+    a += 1;
 })
 
-let b = 0
+var b = 0
 btnPlayer2.addEventListener('click', () => {
-    b += 2
-    player2.textContent = b
+    player2.textContent = b;
+    if(check(b, selectValue)){
+        player2.style.color = 'green';
+        btnPlayer1.classList.add('disabled');
+        btnPlayer2.classList.add('disabled');
+        b = 0
+    }
+    b += 1;
+})
+
+// reset players value
+reset.addEventListener("click", () => {
+    player1.textContent = 0;
+    player2.textContent = 0;
+
+    btnPlayer1.classList.remove('disabled');
+    btnPlayer2.classList.remove('disabled');
+
+    player1.style.color = '';
+    player2.style.color = '';
+
 })
 
 
